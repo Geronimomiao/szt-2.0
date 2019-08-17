e2a11d4b62dd6f503f1d7a46743697e44049442b
wsm <1030057982@qq.com>
2019-08-17 23:43:28 +0800

    目录结构初步完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,6 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
+    
   }
 })
--- utils/router.js
+++ utils/router.js
@@ -1,1 +1,16 @@
 // 页面内所有路由
+
+const routerMap = {
+  'teacher': {
+    'discuss': '/pages/discuss/discuss'
+  }
+}
+
+const router = (state, dest) => {
+ return router[state][dest]
+}
+
+
+module.exports = {
+  router
+}
