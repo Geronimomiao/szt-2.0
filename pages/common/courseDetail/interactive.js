c885b8d3ce592f3a74d21fc2a81ea0ac964f68f9
wsm <1030057982@qq.com>
2019-09-08 07:15:07 +0800

    讨论区 start

--- pages/common/courseDetail/interactive.js
+++ pages/common/courseDetail/interactive.js
@@ -2,4 +2,19 @@ Component({
   options: {
     addGlobalClass: true,
   },
+  data: {
+    card: [{
+      msg: '课程讨论',
+      url: 'https://image.weilanwl.com/color2.0/plugin/wdh2236.jpg'
+    }, {
+      msg: '课程签到',
+      url: 'https://image.weilanwl.com/color2.0/plugin/qpct2148.jpg'
+    }, {
+      msg: '进行评教',
+      url: 'https://image.weilanwl.com/color2.0/plugin/qpczdh2307.jpg'
+    }]
+  },
+  methods: {
+    
+  }
 })
