49c8994541af59cdab59f9024941ef6e56f6ab91
wsm <1030057982@qq.com>
2019-09-07 21:36:11 +0800

    选择题功能 完毕

--- pages/common/courseDetail/comment.js
+++ pages/common/courseDetail/comment.js
@@ -6,9 +6,21 @@ Component({
     addGlobalClass: true,
   },
 
+  data: {
+    index: null,
+    picker: ['心理修养', '道德修养', '思想政治修养', '科学文化修养'],
+  },
+
   methods: {
     toVideo() {
       toNext(router('common', 'video'), 'n')
-    }
+    },
+
+    PickerChange(e) {
+      console.log(e);
+      this.setData({
+        index: e.detail.value
+      })
+    },
   }
 })
