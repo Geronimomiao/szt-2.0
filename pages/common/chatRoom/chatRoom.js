b86fa7cd9da2fe43f21349c64fb6e1422544c3d1
wsm <1030057982@qq.com>
2019-09-23 10:18:09 +0800

    test

--- pages/common/chatRoom/chatRoom.js
+++ pages/common/chatRoom/chatRoom.js
@@ -30,29 +30,36 @@ Page({
 
     wx.onSocketMessage((res) => {
       let msg = JSON.parse(res.data)
-  
+      console.log(msg)
       if (msg.type === 'init') {
         // 获取 client_id
         let client_id = JSON.parse(res.data).client_id
         that.addGroup(client_id)
       } else if (msg.type === 'talk') {
-        // 收到服务端 消息
-        console.log(msg)
+        // 从服务端 收到消息
       }
 
     })
   },
 
   addGroup(client_id) {
+    
     let token = '19d4e8d829eec609990e1e2d84245ef40ec95270'
     let group_id = '1234'
-    
-    // 加入群组
-    app.api.addGroup(token, group_id, client_id)
+
+    app.api.addGroup(token, group_id, client_id).then(res => {
+      this.sendMsg()
+    })
   },
   
   sendMsg() {
+
+    let token = '19d4e8d829eec609990e1e2d84245ef40ec95270'
+    let group_id = '1234'
+    let msg = 'wsm1wsm'
+
     app.api.sendMeg(token, group_id, msg)
+
   }
 
 })
